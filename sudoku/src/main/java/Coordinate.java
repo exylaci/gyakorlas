5a07994fa47c15da67c1de1ccda31e8fc6e6a0ec
exylaci <exy@freemail.hu>
2022-07-05 16:17:01 +0200

    sudoku Coordinate entitás

--- sudoku/src/main/java/Coordinate.java
+++ sudoku/src/main/java/Coordinate.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Coordinate {
+/**
+ * <h1>Beírandó adat pozíciója</h1>
+ * Ebben tárolódik melyik rubrikába kívánja a számot beírni.
+ * Sudokunál ez alapértelmezetten <b>1-9</b> közötti egész szám lehet.
+ *
+ * @param row a sor sorszáma
+ * @param column az oszlop sorszáma
+ */
+public class Coordinate {
+    private int row;
+    private int column;
+
+    public Coordinate(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    @Override
+    public String toString() {
+        return "sor: " + row + " oszlop: " + column;
+    }
 }
